940ce7bc1fdaf4fbc4d0abf01d7b0917686d92c5
davidArce <davidsteven654@gamil.com>
2024-02-04 17:22:50 -0500

    Add coreapi to INSTALLED_APPS and configure REST_FRAMEWORK and react configuration is added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 from tasks import views
+from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
-#esto genera todas las rutas de la api(get, post, put, delete)
+#esto genera todas las rutas de la api
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="Tasks API"))
     
 ]
